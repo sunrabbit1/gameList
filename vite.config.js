ed1d44326ba9cbc124c08b51d43751d300cb06db
洗洗睡 <9767799+wash-and-sleep1@user.noreply.gitee.com>
2025-01-19 00:01:36 +0800

    打包修改

--- vite.config.js
+++ vite.config.js
@@ -15,16 +15,11 @@ export default defineConfig({
         // 移除图片和字体等资源的哈希值
         // assetFileNames: '[name][extname]'
         assetFileNames: (assetInfo) => {
-          // 使用 facadeModuleId 或者 fileName 来判断资源类型
-          const filePath = assetInfo.facadeModuleId || assetInfo.fileName;
-
-          if (typeof filePath === 'string' && /\.(png|jpe?g|gif|svg)$/.test(filePath)) {
-            // 如果是图片文件，则不添加哈希值
-            return `${filePath.match(/([^/\\]+)\.\w+$/)[1]}.${filePath.split('.').pop()}`;
+            if (assetInfo.name && /\.(png|jpe?g|gif|svg)$/.test(assetInfo.name)) {
+              return `assets/[name][extname]`; // 仅适用于图片文件
+            }
+            return '[name]-[hash][extname]'; // 其他资源保持哈希
           }
-          // 对于其他资源，保留默认行为（包括哈希）
-          return '[name]-[hash][extname]';
-        }
       }
     }
   },
