dc59c7317ca134f888382820c0bf2fdf3af1e48c
liuby <liuby@hzyb.com>
2025-03-28 11:42:07 +0800

    回退

--- src/view/GameCard.vue
+++ src/view/GameCard.vue
@@ -75,7 +75,6 @@ const sortLikeWin = (name1, name2) => {
   return compareValue
 }
 
-console.log(import.meta.env);
 onMounted(async () => {
   try {
     const loadedImages = list.sort((a, b) => sortLikeWin(a.gameName, b.gameName));
