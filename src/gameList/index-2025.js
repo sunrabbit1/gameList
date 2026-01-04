63e0c8928bd2fa123620498d1fad9e1203b90346
liuby <liuby@hzyb.com>
2026-01-04 17:30:01 +0800

    2025 1-2月份

--- src/gameList/index-2025.js
+++ src/gameList/index-2025.js
@@ -1,35 +1,146 @@
 export const list = {
   '一月': [
     {
-      "fileName": "image-75.jpg",
-      "gameName": "大头菜小子逃税记",
-      "url": "https://s2.loli.net/2025/01/30/7HBPvYSpjQ3RcDq.jpg"
+      "fileName": "image-95.jpg",
+      "gameName": "怪物猎人：世界",
+      "url": "https://s2.loli.net/2025/01/30/4cJgr1zWHn7vDR6.jpg"
+    },
+    {
+      "fileName": "image-180.jpg",
+      "gameName": "时光之刃",
+      "url": "https://s2.loli.net/2025/01/30/SGJo2FmZC5lqeON.jpg"
+    },
+    {
+      "fileName": "image-167.jpg",
+      "gameName": "闰跃之年",
+      "url": "https://s2.loli.net/2025/01/30/FDcjOXMKR9bmECe.jpg"
+    },
+    {
+      "fileName": "image-74.jpg",
+      "gameName": "大神绝景版",
+      "url": "https://s2.loli.net/2025/01/30/5SUqkWPxGR63Qjh.jpg"
+    },
+    {
+      "fileName": "image-175.jpg",
+      "gameName": "射戮骑士",
+      "url": "https://s2.loli.net/2025/01/30/OBcz4wXfs7oMQGn.jpg"
+    },
+    {
+      "fileName": "image-179.jpg",
+      "gameName": "师父",
+      "url": "https://s2.loli.net/2025/01/30/sahO5EHokWBySJe.jpg"
+    },
+    {
+      "fileName": "image-11.jpg",
+      "gameName": "Curse Crackers",
+      "url": "https://s2.loli.net/2025/01/30/7waX64ld83eufjb.jpg"
+    },
+    {
+      "fileName": "image-223.jpg",
+      "gameName": "幽灵行者1",
+      "url": "https://s2.loli.net/2025/01/31/SCTfnVkXU15ZbFz.jpg"
+    },
+    {
+      "fileName": "image-219.jpg",
+      "gameName": "银白钢铁伊克斯1",
+      "url": "https://s2.loli.net/2025/01/31/ATSDMaWXb3Zjxuy.jpg"
+    },
+    {
+      "fileName": "image-65.jpg",
+      "gameName": "苍翼：混沌效应",
+      "url": "https://s2.loli.net/2025/01/30/3GuzIHkyfB4QSE6.jpg"
+    },
+    {
+      "fileName": "image-99.jpg",
+      "gameName": "合金装备崛起：复仇",
+      "url": "https://s2.loli.net/2025/01/30/NL9tCUofm8RXYnH.jpg"
+    },
+    {
+      "fileName": "image-142.jpg",
+      "gameName": "倾听画语：最美好的景色",
+      "url": "https://s2.loli.net/2025/01/30/FyDvt3IRG6wNoMB.jpg"
+    },
+    {
+      "fileName": "image-224.jpg",
+      "gameName": "渔帆暗涌",
+      "url": "https://s2.loli.net/2025/01/31/SyYxCGKwgmrHlsk.jpg"
+    },
+    {
+      "fileName": "image-248.jpg",
+      "gameName": "小绵羊的朝圣之路",
+      "url": "https://s2.loli.net/2025/01/31/9QMfZlnXwkpy1tV.jpg"
     },
   ],
   '二月': [
     {
-      "fileName": "image-214.jpg",
-      "gameName": "心渊梦境",
-      "url": "https://s2.loli.net/2025/01/31/amQzZchdUL34vtj.jpg"
+      "fileName": "image-249.jpg",
+      "gameName": "莫莫多拉1",
+      "url": "https://s2.loli.net/2025/02/01/JUoqAsuByl3fj2N.jpg"
     },
     {
-      "fileName": "image-176.png",
-      "gameName": "神之天平DLC",
-      "url": "https://s2.loli.net/2025/01/30/cZ4UAt9a8soVNEQ.png"
-    }
-  ],
-  '三月': [
+      "fileName": "image-250.jpg",
+      "gameName": "莫莫多拉2",
+      "url": "https://s2.loli.net/2025/02/01/jqgLtQheyplowOz.jpg"
+    },
     {
-      "fileName": "image-136.jpg",
-      "gameName": "女神异闻录5皇家版",
-      "url": "https://s2.loli.net/2025/01/30/TdbWicEaDOZM42Y.jpg"
-    }
-  ],
-  '四月': [
+      "fileName": "image-65.jpg",
+      "gameName": "苍翼：混沌效应",
+      "url": "https://s2.loli.net/2025/01/30/3GuzIHkyfB4QSE6.jpg"
+    },
     {
-      "fileName": "image-209.jpg",
-      "gameName": "小岛",
-      "url": "https://s2.loli.net/2025/01/31/1THc3vkxNbpaIfP.jpg"
-    }
-  ]
+      "fileName": "image-95.jpg",
+      "gameName": "怪物猎人：世界",
+      "url": "https://s2.loli.net/2025/01/30/4cJgr1zWHn7vDR6.jpg"
+    },
+    {
+      "fileName": "",
+      "gameName": "北方之境",
+      "url": ""
+    },
+    {
+      "fileName": "",
+      "gameName": "遗迹1",
+      "url": ""
+    },
+    {
+      "fileName": "",
+      "gameName": "怪物猎人：荒野(demo)",
+      "url": ""
+    },
+    {
+      "fileName": "",
+      "gameName": "兽人必须死3",
+      "url": ""
+    },
+    {
+      "fileName": "image-20.png",
+      "gameName": "Q立方",
+      "url": "https://s2.loli.net/2025/01/30/QKqBFmnJdDp9bSc.png"
+    },
+    {
+      "fileName": "",
+      "gameName": "LEGO®Builder's Journey",
+      "url": ""
+    },
+    {
+      "fileName": "image-253.jpg",
+      "gameName": "Jaded",
+      "url": "https://s2.loli.net/2025/12/03/4K5NHOIUtph1vq2.jpg"
+    },
+    {
+      "fileName": "",
+      "gameName": "幽灵线：东京",
+      "url": ""
+    },
+    {
+      "fileName": "",
+      "gameName": "瘟疫传说1",
+      "url": ""
+    },
+    {
+      "fileName": "image-94.jpg",
+      "gameName": "怪物猎人：崛起",
+      "url": "https://s2.loli.net/2025/01/30/xh4WEvlZIF6Xk5K.jpg"
+    },
+  ],
 };
